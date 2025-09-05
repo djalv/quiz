e123b79b3a488e81f59eb213667ff4b179d19b4a
djalv <alvaroneto98@protonmail.com>
2025-09-05 20:27:32 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -34,3 +34,102 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+def test_create_multiple_choices():
+    question = Question(title='q1')
+
+    question.add_choice('a')
+    question.add_choice('b', True)
+
+    assert len(question.choices) == 2
+
+    assert question.choices[0].text == 'a'
+    assert not question.choices[0].is_correct
+
+    assert question.choices[1].text == 'b'
+    assert question.choices[1].is_correct
+
+def test_remove_choice():
+    question = Question(title='q1')
+    
+    question.add_choice('a')
+
+    choice = question.choices[0]
+    choice_id = choice.id
+    
+    question.remove_choice_by_id(choice_id)
+    assert len(question.choices) == 0
+
+def test_remove_non_existent_choice():
+    question = Question(title='q1')
+    
+    question.add_choice('a')
+    
+    with pytest.raises(Exception, match='Invalid choice id 999'):
+        question.remove_choice_by_id(999)
+
+def test_remove_all_choices():
+    question = Question(title='q1')
+
+    question.add_choice('a')
+    question.add_choice('b', True)
+
+    question.remove_all_choices()
+    assert len(question.choices) == 0
+
+def test_set_correct_choices():
+    question = Question(title='q1', max_selections=2)
+
+    question.add_choice('a')
+    question.add_choice('b')
+    question.add_choice('c')
+    question.add_choice('d')
+
+    question.set_correct_choices([question.choices[1].id, question.choices[3].id])
+
+    assert not question.choices[0].is_correct
+    assert question.choices[1].is_correct
+    assert not question.choices[2].is_correct
+    assert question.choices[3].is_correct
+
+def test_correct_selected_choices():
+    question = Question(title='q1', max_selections=2)
+    
+    question.add_choice('a')
+    question.add_choice('b', True)
+    question.add_choice('c')
+    question.add_choice('d', True)
+
+    selected_ids = [question.choices[1].id, question.choices[3].id]
+    correct_selected_ids = question.correct_selected_choices(selected_ids)
+
+    assert len(correct_selected_ids) == 2
+    assert question.choices[1].id in correct_selected_ids
+    assert question.choices[3].id in correct_selected_ids
+
+def test_correct_selected_choices_exceeding_max_selections():
+    question = Question(title='q1')
+
+    question.add_choice('a')
+    question.add_choice('b', True)
+
+    with pytest.raises(Exception, match='Cannot select more than 1 choices'):
+        question.correct_selected_choices([question.choices[0].id, question.choices[1].id])
+
+def test_add_choice_with_invalid_text():
+    question = Question(title='q1')
+
+    with pytest.raises(Exception, match='Text cannot be empty'):
+        question.add_choice('')
+
+def test_set_non_existent_choice_as_correct():
+    question = Question(title='q1')
+    
+    question.add_choice('a')
+
+    with pytest.raises(Exception, match='Invalid choice id -1'):
+        question.set_correct_choices([-1])
+
+def test_create_question_with_empty_title():
+    with pytest.raises(Exception, match='Title cannot be empty'):
+        question = Question(title='')
